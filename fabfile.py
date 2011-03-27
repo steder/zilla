727f346ac14e9005d6de8c080b151e0bf077115c
Mike Steder <steder@gmail.com>
2011-03-27 16:59:05 -0500

    Added nosy

--- fabfile.py
+++ fabfile.py
@@ -3,10 +3,19 @@ from fabric.api import local #, run
 def static():
     local("python zilla/manage.py collectstatic --noinput")
 
-test_command = """DJANGO_SETTINGS_MODULE=zilla.settings PYTHONPATH="." %s --with-coverage --cover-package=zilla --with-django zilla --nocapture"""
+
 
 def test():
-    local(test_command % ("nosetests",))
+    test_command = """DJANGO_SETTINGS_MODULE=zilla.settings PYTHONPATH="." nosetests --with-django zilla --nocapture"""
+    local(test_command)
+
+def coverage():
+    coverage_command = """DJANGO_SETTINGS_MODULE=zilla.settings PYTHONPATH="." nosetests --with-coverage --cover-package=zilla --with-django zilla --nocapture"""
+    local(coverage_command)
+
+def nosy():
+    command = """DJANGO_SETTINGS_MODULE=zilla.settings PYTHONPATH="." nosy -c  etc/nosy.cfg"""
+    local(command)
 
 def twisted_dev():
     static()
