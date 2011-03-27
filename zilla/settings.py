72d91bdb72969b3c507e9e0335f458ff922063a0
Mike Steder <steder@gmail.com>
2011-03-26 22:05:35 -0500

    Fixed TEMPLATE_DIRS

--- zilla/settings.py
+++ zilla/settings.py
@@ -154,7 +154,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    ZILLA_ROOT.child("zilla").child("templates"),
+    TEMPLATE_DIR,
 )
 
 INSTALLED_APPS = (
