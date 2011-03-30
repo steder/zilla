9050a83c8f79d863ea50dca6cda2d14607986410
Mike Steder <steder@gmail.com>
2011-03-30 16:23:07 -0500

    Added test for m3u resources

---
+++ zilla/jukebox/test_m3u.py
@@ -0,0 +1,11 @@
+from django import test as unittest
+
+from zilla import m3u
+
+
+class TestM3U(unittest.TestCase):
+    def test_jukebox_m3u(self):
+        """The streaming urls for all songs in the jukebox should be included in the m3u generated from accessing this url."""
+        response = self.client.get("/jukebox/m3u")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, "/streaming/song/1\n/streaming/song/2")
